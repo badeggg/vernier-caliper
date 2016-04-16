b2945fc180febdabf35a971d5ab7a55b6d85e82e
badeggg <zhaoxuxujc@163.com>
2016-04-16 12:10:03 +0800

    add server code

---
+++ app.js
@@ -0,0 +1,15 @@
+﻿/**
+ * @badeggg
+ * 2016.4.16 start coding
+ * 开始
+ */
+var express = require('express');
+var path = require('path');
+
+var app = express();
+app.use( express.static(__dirname) );
+app.get('/', function(req, res){
+  res.sendFile( path.join(__dirname, 'home.html') );
+});
+app.listen(3000);
+console.log('vernier-caliper server running...');
